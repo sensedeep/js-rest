4d0757ef2d47ff42eed6cab209320e35026fddf0
Michael O'Brien <mob@emobrien.com>
2020-11-09 12:17:28 +1100

    DEV

--- index.js
+++ index.js
@@ -49,7 +49,6 @@
  */
 
 import Net from 'js-net'
-import Clone from 'js-clone'
 import Blend from 'js-blend'
 import UUID from 'js-uuid'
 
@@ -73,23 +72,32 @@ const SingletonRest = {
     update: { method: 'POST',  uri: '/:controller/update' },
 }
 
+const Log = {
+    info(...args) { console.log(...args) },
+    error(...args) { console.log(...args) },
+    exception(...args) { console.log(...args) },
+    trace(...args) { console.log(...args) },
+}
+
 export default class Rest {
     /*
         WARNING: API keys become top-level instance properties
      */
     constructor(name, customApis = {}, modifiers = {group: true}) {
         this._net = new Net
-        this._name = name
+        this._name = name.toLowerCase()
         this._model = toTitle(name)
         this._clientId = UUID()
-        modifiers = this.modifiers = Object.assign({}, modifiers)
+        modifiers = this._modifiers = Object.assign({}, modifiers)
+        modifiers.service = modifiers.service || ''
+        this._service = modifiers.service || ''
         let base
         if (modifiers.base == 'singleton') {
             base = SingletonRest
         } else if (modifiers.base != 'none') {
             base = GroupRest
         }
-        this._apis = Blend(Clone(base), customApis)
+        this._apis = Blend(Object.clone(base), customApis)
 
         for (let [action,api] of Object.entries(this._apis)) {
             if (typeof api == 'function') {
@@ -105,21 +113,43 @@ export default class Rest {
                 if (api.putMap === undefined && !api.nomap) {
                     api.putMap = modifiers.putMap
                 }
-                if (api.context === undefined) {
-                    api.context = modifiers.context
-                }
+            }
+            if (!customApis[action] && api.context === undefined) {
+                api.context = modifiers.context
             }
         }
     }
 
+    static setConfig(callback, settings = {}) {
+        Rest.settings = settings
+        Rest.config = settings.config
+        Rest.version = settings.version
+        Rest.callback = callback
+    }
+
+    async invoke(reason, args) {
+        let callback = Rest.callback
+        if (callback) {
+            args.controller = this._name
+            args.model = this._model
+            args.cache = this._modifiers.cache
+            return await callback(reason, args)
+        }
+        return true
+    }
+
     createAction(api, action) {
         return async function(fields, options = {}) {
-            let {args, uri} = await this.prepRemote(api, fields, options)
-            let mark = new Date
-            let result = await this._net.fetch(uri, args)
-            let elapsed = (new Date() - mark) / 1000
-            if (result && api.getMap && !api.nomap) {
-                result = await api.getMap(result)
+            let {fetch, result} = await this.invoke('before', {action, fields, options})
+            if (fetch) {
+                let {args, uri} = await this.prepRemote(api, fields, options)
+                let mark = new Date
+                result = await this._net.fetch(uri, args)
+                let elapsed = (new Date() - mark) / 1000
+                if (result && api.getMap && !api.nomap) {
+                    result = await api.getMap(result)
+                }
+                result = await this.invoke('after', {action, fields, options, result})
             }
             return result
         }
@@ -131,20 +161,27 @@ export default class Rest {
          */
         let action = api.action
         let name = this._name
+        let service = this._service
+        let serviceOverride = false
         let uri = api.uri.replace(/:\w*/g, function(match, lead, tail) {
-            let controller = match.slice(1)
-            if (controller == 'controller') {
-                return name.toLowerCase()
+            let field = match.slice(1)
+            if (field == 'controller') {
+                return name
+            }
+            if (field == 'service') {
+                serviceOverride = true
+                return service
             }
             return (fields && fields[controller]) ? fields[controller] : ''
         })
+        if (!serviceOverride && this._service) {
+            uri = this._service + uri
+        }
         let args = Object.assign(Object.assign({}, api), options)
         let body = {fields: {}}
         if (fields) {
             for (let [field,value] of Object.entries(fields)) {
-                if (value != null) {
-                    body.fields[field] = value
-                }
+                body.fields[field] = value
             }
             if (body.fields && api.putMap && !api.nomap) {
                 body.fields = await api.putMap(body.fields)
@@ -156,8 +193,17 @@ export default class Rest {
         if (app.state.app.logging) {
             options.logging = options.logging || app.state.app.logging
         }
+        if (app.state.auth.assume) {
+            options.assume = options.assume || app.state.auth.assume
+        }
         options.clientId = this._clientId
-        body.options = Object.white(options, ['clientId', 'offset', 'limit', 'filter', 'logging'])
+        if (app.state.auth.apiToken) {
+            body.token = app.state.auth.apiToken
+        }
+        body.options = Object.white(options, [
+            'assume', 'clientId', 'create', 'offset', 'limit', 'filter', 'logging'
+        ])
+        body.version = Rest.version
 
         if (Object.keys(body).length > 0) {
             if (args.method != 'POST') {
@@ -179,7 +225,7 @@ export default class Rest {
         if (app.state.auth.tokens) {
             args.headers.Authorization = app.state.auth.tokens.idToken.jwtToken
         }
-        args.base = app.config.api
+        args.base = Rest.config.api
         return {args, uri}
     }
 }
